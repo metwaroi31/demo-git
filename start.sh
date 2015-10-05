61766a71ae2b2c1cb2b5ca26a2251af95be492ff
Wojciech Frącz <fraczwojciech@gmail.com>
2015-10-05 10:39:25 +0200

    Ex: commit-lost

--- start.sh
+++ start.sh
@@ -18,3 +18,8 @@ echo ",----------[----------------------.,----------]" >> program.txt
 echo ",>,>++++++++[<------<------>>-]." >> program.txt
 echo "<<[>[>+>+<<-]>>[<<+>>-]<<<-]" >> program.txt
 echo ">>>++++++[<++++++++>-]<." >> program.txt
+echo "This is the good version of a file." > file.txt
+git add file.txt
+git commit -m "Very imporant piece of work"
+echo "I have accidentally deleted my work!" > file.txt
+git commit -a --amend -m "Accidental change"
